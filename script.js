5760b4e53d7d4b731d734e2ec21be06be37751ee
Sravan S Nair <84452323+livelysravan@users.noreply.github.com>
2024-10-04 14:53:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll("img");
+    images.forEach(image => {
+        image.addEventListener("mouseover", () => {
+            image.style.transform = "scale(1.05)";
+            image.style.transition = "transform 0.3s";
+        });
+
+        image.addEventListener("mouseout", () => {
+            image.style.transform = "scale(1)";
+        });
+    });
+});
